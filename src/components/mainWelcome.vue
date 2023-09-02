932881c095fd6d98ca6666237d2f4280dbf615cb
Logan Eastman <eastmanlg@gmail.com>
2023-09-02 14:00:24 -0600

    Rename HelloWorld > mainWelcome

---
+++ src/components/mainWelcome.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+v-container.fill-height
+    v-responsive.align-center.text-center.fill-height
+        v-img(height="300" src="@/assets/logo.svg")
+        span.text-body-2.font-weight-light.mb-n1 Welcome to
+        h1.pb-10.text-h2.font-weight-bold Hotbox
+        v-row.d-flex.align-center.justify-center
+            v-col(cols="auto")
+                v-btn(href="https://vuetifyjs.com/components/all/" min-width="164" rel="noopener noreferrer" target="_blank" variant="text")
+                    v-icon(icon="mdi-cog" size="large" start="")
+                    | Settings
+            v-col(cols="auto")
+                v-btn(color="primary" href="https://vuetifyjs.com/introduction/why-vuetify/#feature-guides" min-width="228" rel="noopener noreferrer" size="x-large" target="_blank" variant="flat")
+                    v-icon(icon="mdi-radiator" size="large" start="")
+                    | Start Roasting
+            v-col(cols="auto")
+                v-btn(href="https://community.vuetifyjs.com/" min-width="164" rel="noopener noreferrer" target="_blank" variant="text")
+                    v-icon(icon="mdi-account-group" size="large" start="")
+                    | Community
+</template>
+
+<script setup>
+  //
+</script>
