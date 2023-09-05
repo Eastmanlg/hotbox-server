543544cbd5bab35e295c1ed9dc2e67d643454e4e
Logan Eastman <eastmanlg@gmail.com>
2023-09-04 21:24:12 -0600

    Edit connection component to use device store

---
+++ src/components/debuggerConnection.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+v-card
+    v-card-title Controller Connection
+    v-card-text
+        h1.ma-4 {{ device.count }}
+        v-btn(@click='increment()') Increment
+        v-btn(@click='reset()') Reset
+</template>
+
+<script setup>
+// import {debounce} from 'lodash';
+import { useDeviceStore } from '@/store/device';
+const device = useDeviceStore();
+
+function increment(){
+    device.increment(2);
+}
+
+function reset() {
+    device.reset();
+}
+
+</script>
+
+<!-- <style lang="scss">
+</style> -->
