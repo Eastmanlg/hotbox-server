15f53cd44461aa8315c5e6317022f2027c27740e
Logan Eastman <eastmanlg@gmail.com>
2023-09-28 16:13:43 -0600

    remove johnny-five import

--- server/controller.js
+++ server/controller.js
@@ -1,13 +1,6 @@
-import j5 from 'johnny-five';
 
 export default class Controller {
     constructor(port) {
-        this.board = new j5.Board({port, repl:false});
-        this.led = {};
-
-        this.board.on('ready', () => {
-            this.led = new j5.Led(13);
-        });
     }
 
     async blink(ms) {
