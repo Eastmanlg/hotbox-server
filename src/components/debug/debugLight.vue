0ecae8ea0a403fe52202d9dc736585dc340c8408
Logan Eastman <eastmanlg@gmail.com>
2023-09-30 13:34:28 -0600

    move light debugger to debugger folder

--- src/components/debug/debugLight.vue
+++ src/components/debug/debugLight.vue
@@ -5,9 +5,9 @@ v-card(max-width='20vw', elevation="11")
         v-container()
             v-row(dense)
                 v-col(cols='6')
-                    v-btn(@click="on()", prepend-icon='mdi-lightbulb-off', variant="text") Off
+                    v-btn(@click="off()", prepend-icon='mdi-lightbulb-off', variant="text") Off
                 v-col(cols='6')
-                    v-btn(@click="off()", prepend-icon='mdi-lightbulb-on', color="secondary", variant="text") On
+                    v-btn(@click="on()", prepend-icon='mdi-lightbulb-on', color="secondary", variant="text") On
     v-card-actions 
 </template>
 
