2749b9d72750704665942a066e4ca82e9ba942eb
Logan Eastman <eastmanlg@gmail.com>
2023-09-30 13:26:17 -0600

    add debugLight component

---
+++ src/components/debugLight.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+v-card(max-width='20vw', elevation="11")
+    v-card-title Debug Light
+    v-card-text
+        v-container()
+            v-row(dense)
+                v-col(cols='6')
+                    v-btn(@click="on()", prepend-icon='mdi-lightbulb-off', variant="text") Off
+                v-col(cols='6')
+                    v-btn(@click="off()", prepend-icon='mdi-lightbulb-on', color="secondary", variant="text") On
+    v-card-actions 
+</template>
+
+<script setup>
+import { socket } from '@/socket';
+
+function on() {
+    socket.emit('debug-lightOn');
+}
+
+function off() {
+    socket.emit('debug-lightOff');
+}
+</script>
+
+<!-- <style lang="scss">
+</style> -->
